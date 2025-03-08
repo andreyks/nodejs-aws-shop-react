3878ec8d32eb1b16bf7655cdb2905c2232e9221d
Andrii Kostromin <andrii_kostromin@epam.com>
2025-03-09 00:46:13 +0300

    Task 5: frontent import url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://am2xsc1rw3.execute-api.eu-north-1.amazonaws.com/development",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://jbuqseyfkg.execute-api.eu-north-1.amazonaws.com/development",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
