728ea947ffd2a8f7d2b761b68a9bb919a02a63e0
Andrii Kostromin <andrii_kostromin@epam.com>
2025-04-16 16:48:18 +0300

    Task 09: Cart API docker

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://jbuqseyfkg.execute-api.eu-north-1.amazonaws.com/development",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  // cart: "https://busulrg7wvplj7l2pwxovdi7lq0ucyme.lambda-url.eu-north-1.on.aws",
+  cart: "https://andreyks-cart-api-dev.eu-north-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
