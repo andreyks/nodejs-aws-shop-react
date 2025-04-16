62dc0d4c107fbe000833e7c04a4df2b1e1a79611
Andrii Kostromin <andrii_kostromin@epam.com>
2025-04-16 21:06:14 +0300

    Task 09: Cart API (fix db query for Task 08)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,10 @@
 const API_PATHS = {
   product: "https://cj1fjt38ve.execute-api.eu-north-1.amazonaws.com/development",
-  order: "https://andreyks-cart-api-dev.eu-north-1.elasticbeanstalk.com",
+  order: "https://andreyks-cart-api-dev.eu-north-1.elasticbeanstalk.com/api/profile/cart",
   import: "https://jbuqseyfkg.execute-api.eu-north-1.amazonaws.com/development",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   // cart: "https://busulrg7wvplj7l2pwxovdi7lq0ucyme.lambda-url.eu-north-1.on.aws",
-  cart: "https://andreyks-cart-api-dev.eu-north-1.elasticbeanstalk.com",
+  cart: "https://andreyks-cart-api-dev.eu-north-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
